ab24dbf5370fd5cd1785ae60d86ee73b1a0462e5
kranz <kranz@kranz.cloud>
2017-11-18 15:21:41 +0100

    Hello World component running

---
+++ app/components/HelloWorld.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class HelloWorld extends React.Component {
+	render() {
+		return (
+			<div id="ciccio">
+				<p>Una versione di latino al giorno</p>
+				<p>leva l'insufficienza di torno</p>
+			</div>
+		)
+	}
+}
+
+module.exports = HelloWorld;
